e2daafa534785680998d01fc8315d1014e61b1e9
nishantprajapati123 <knishant147@gmail.com>
2020-06-06 17:30:31 +0530

    Week 1

---
+++ Week 1/QueueReconstructionByHeight.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        
+        sort(people.begin(), people.end(), [](vector<int>&p1, vector<int>&p2)
+             {
+                if(p1[0] == p2[0])  return p1[1] > p2[1];
+                 return p1[0] < p2[0];
+             });
+        vector<vector<int> >ans;
+        for(int i=people.size()-1; i>=0;i--)
+            ans.insert(ans.begin() + people[i][1], people[i]);
+        
+        return ans;
+    }
+};
