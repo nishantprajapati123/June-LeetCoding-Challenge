594599562863485aa1518cf9300577d1a084cabe
nishantprajapati123 <knishant147@gmail.com>
2020-06-08 23:36:33 +0530

    modified

--- Week 2/PowerOfTwo.cpp
+++ Week 2/PowerOfTwo.cpp
@@ -1,82 +1,14 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-#define ll 				long long int
-#define pb 				push_back
-#define mk 				make_pair
-#define ff 				first
-#define ss 				second
-#define deb(x) 			cout << #x << "=" << x << endl
-#define all(x) 			x.begin(), x.end()
-#define clr(x) 			memset(x, 0, sizeof(x))
-#define sortall(x) 		sort(all(x))
-#define PI 				3.1415926535897932384626
-#define MOD 			1000000007
-#define INF 			(ll)1e30
-#define fastIO 			ios::sync_with_stdio(0); cin.tie(0);	cout.tie(0);
-
-typedef pair<int, int> 	pii;
-typedef pair<ll, ll> 	pll;
-typedef vector<int> 	vi;
-typedef vector<ll> 		vl;
-typedef vector<pii> 	vpii;
-typedef vector<pll> 	vpll;
-typedef vector<vi> 		vvi;
-typedef vector<vl> 		vvl;
-
-int pow(int base, int exp);
-// ============================
-
-void solve()
-{
-    int n;
-    cin>>n;
-    vi a(n), b(n);
-    for(int i=0;i<n;i++)    cin>>a[i];
-    for(int i=0;i<n;i++)    cin>>b[i];
-
-    int ans1 = 0, ans2 = 0, ans3=1, ans4=1, initial = 0;
-    for(int i=0;i<n;i++)
-    {
-        if(a[i] == b[i])    initial++;
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        
+        int count = 0;
+        while(n > 0)
+        {
+            count++;
+            n = n & (n-1);
+        }
+        
+        return count == 1;
     }
-    int i=0, j=0;
-    for(;i<n && a[i]!=1;i++);
-    for(;j<n && b[j]!=1;j++);
-
-    for(int k=0;k<n;k++,i++,j++)
-    {
-        if(a[i%n] == b[j%n])    ans1++;
-    }
-    cout<<max({initial, ans1})<<"\n";
-    // cout<<i<<" "<<j<<endl;
-}
-int main()
-{
-    fastIO
-    // ONLINE_JUDGE
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
-
-    int T = 1;
-    // cin>>T;
-    for(int testcase = 1; testcase<=T; testcase++)
-    {
-        solve();
-    }
-    return 0;
-}
-
-int pow(int base, int exp) {
-    base %= MOD;
-    int result = 1;
-    while (exp > 0) 
-    {
-        if (exp & 1) result = ((ll)result * base) % MOD;
-        base = ((ll)base * base) % MOD;
-        exp >>= 1;
-    }
-    return result;
-}
+};
