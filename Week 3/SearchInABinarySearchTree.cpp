ec29c719ed88fb7f8b360432ac81430bd5ffd23d
nishantprajapati123 <knishant147@gmail.com>
2020-06-15 16:40:48 +0530

    Week 3

---
+++ Week 3/SearchInABinarySearchTree.cpp
@@ -0,0 +1,28 @@
+// Time complexity :- O(log(n)) / O(n) (skewed tree)
+// Space complexity :- O(1) / O(log(n)) (stack space)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        
+        if(root == NULL)
+            return NULL;
+        
+        if(root->val == val)
+            return root;
+        else if(root->val > val)
+            return searchBST(root->left, val);
+        else
+            return searchBST(root->right, val);
+    }
+};
