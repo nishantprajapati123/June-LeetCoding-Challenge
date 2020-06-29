a1d9818d9ee7394f633f04d0d37e8205d1b774b7
nishantprajapati123 <knishant147@gmail.com>
2020-06-29 21:27:31 +0530

    Wek 5

---
+++ Week 5/UniquePaths.cpp
@@ -0,0 +1,15 @@
+// TIme complexity :- O(m*n)
+// Space complexity :- O(m*n)
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        if(m == 0)  return 0;
+        vector<vector<int> >dp(m, vector<int>(n,1));
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+                dp[i][j] = dp[i][j-1] + dp[i-1][j];
+        }
+        return dp[m-1][n-1];
+    }
+};
